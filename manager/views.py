b3e6a4520ebd9c5a052b1e5be06ce4fd3c1d11c1
Rummage777 <777.free.writer@gmail.com>
2019-08-13 20:31:17 +0300

    Clean repo

--- manager/views.py
+++ manager/views.py
@@ -1,1 +1,33 @@
-from django.views.generic.base import TemplateViewfrom .models import Disciplinefrom .models import Student# My pages views hereclass HomeView(TemplateView):    template_name = 'home.html'class StudentsListView(TemplateView):    template_name = 'students.html'    def get_context_data(self, **kwargs):        context = super().get_context_data(**kwargs)        context['student_list'] = Student.objects.all()        return contextclass DisciplinesListView(TemplateView):    template_name = 'disciplines.html'    def get_context_data(self, **kwargs):        context = super().get_context_data(**kwargs)        context['discipline_list'] = Discipline.objects.all()        return context
+from django.views.generic.base import TemplateView
+from .models import Discipline
+from .models import Student
+
+
+# My pages views here
+
+class HomeView(TemplateView):
+
+    template_name = 'home.html'
+
+
+class StudentsListView(TemplateView):
+
+    template_name = 'students.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['student_list'] = Student.objects.all()
+        return context
+
+
+class DisciplinesListView(TemplateView):
+
+    template_name = 'disciplines.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['discipline_list'] = Discipline.objects.all()
+        return context
+
